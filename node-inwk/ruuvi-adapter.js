fe2d5b6d4862373c087c1e66facc3b22600ce2ff
Maen Artimy <artimym@gmail.com>
2019-05-05 19:13:45 -0300

    handling empty input object

--- node-inwk/ruuvi-adapter.js
+++ node-inwk/ruuvi-adapter.js
@@ -5,13 +5,17 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
             // expects the msg.payload to be as JSON-formatted string 
             var jmsg = JSON.parse(msg.payload);
-            var msg1 = {payload: jmsg.temperature};
-            var msg2 = {payload: jmsg.humidity};
-            var msg3 = {payload: jmsg.pressure};
-            var msg4 = {payload: jmsg.acceleration};
-            var msg5 = {payload: jmsg.battery};
             
-            node.send([msg1, msg2, msg3, msg4, msg5]);
+            // Check if the object is empty.
+            if(jmsg.hasOwnProperty('temperature')) { 
+                var msg1 = {payload: jmsg.temperature};
+                var msg2 = {payload: jmsg.humidity};
+                var msg3 = {payload: jmsg.pressure};
+                var msg4 = {payload: jmsg.acceleration};
+                var msg5 = {payload: jmsg.battery};
+                
+                node.send([msg1, msg2, msg3, msg4, msg5]);
+            }
         });
     }
     RED.nodes.registerType("ruuvi-adapter",RuuviAdapterNode);
