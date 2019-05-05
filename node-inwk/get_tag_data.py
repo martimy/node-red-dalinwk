2cbb04d94513cc4202b903859b3e81783a404046
Maen Artimy <artimym@gmail.com>
2019-05-05 18:40:07 -0300

    adding timeout for reading tag data

--- node-inwk/get_tag_data.py
+++ node-inwk/get_tag_data.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
-from ruuvitag_sensor.ruuvitag import RuuviTag
+
+# Form: https://github.com/ttu/ruuvitag-sensor
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
 import json, sys, re
 
-def run(mac):
-    sensor = RuuviTag(mac)
-    sensor.update()
-    return sensor.state
+
+def run(mac, timeout=3):
+    # This can handle more than one MAC address
+    return RuuviTagSensor.get_data_for_sensors([mac], timeout)
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: python3 get_tag_data.py <MAC Address of the RuuviTag>")
     elif re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", sys.argv[1].lower()):
-        state = run(sys.argv[1])
-        print(json.dumps(state))
+        MAC = sys.argv[1]
+        if len(sys.argv) > 2 and sys.argv[2].isdigit():
+            timeout = int(sys.argv[2]) 
+            data = run(MAC, timeout)
+        else:
+            data = run(MAC)
+
+        print(json.dumps(data.get(MAC, {})))
     else:
         print("Invalid MAC address %s" % sys.argv[1])
         
--- node-inwk/ruuvi-scan.js
+++ node-inwk/ruuvi-scan.js
@@ -2,12 +2,15 @@ module.exports = function(RED) {
     function RuuviScanNode(config) {
         RED.nodes.createNode(this,config);
         this.mac = config.mac;
+        this.timeout = config.timeout;
         var node = this;
 
         node.on('input', function(msg) {
 
             const { spawn } = require('child_process');
-            const py = spawn("python", ['/home/pi/node-red-dalinwk/node-inwk/get_tag_data.py', this.mac]);
+            const py = spawn("python", ['/home/pi/node-red-dalinwk/node-inwk/get_tag_data.py', this.mac, this.timeout]);
+
+            //setTimeout(function(){ py.kill()}, this.timeout);
 
             py.stdout.on('data', function(data){
               msg.payload = data.toString();
